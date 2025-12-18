3e0731ea90ab5608990e371d5340fb331ab95b79
Dan C <dan@e-dan.co.uk>
2025-12-18 22:50:57 +0000

    Initial testing

---
+++ src/main.cpp
@@ -0,0 +1,250 @@
+/*
+ * main.cpp
+ *
+ * A hot water timer with web control using Mongoose Application to pass back static web pages and handle rest requests and websocket
+ * Using Mongoose TCPIP stack and Drivers
+ * Listen on port 80
+ *  Created on: November 2025
+ *      Author: electro-dan
+ */
+
+/* TODO
+
+Add time request
+Add flash save / load
+
+*/
+
+#include <cstdio>
+#include <pico/stdlib.h>
+#include "pico/util/datetime.h"
+#include <time.h>
+#include "hardware/rtc.h"
+
+#include "mongoose.h"
+
+#define HTTP_URL "http://0.0.0.0:80"
+
+struct mg_mgr g_mgr;
+
+uint64_t sntp_refresh_counter = 0;
+bool sntp_refresh_required = true;
+
+// SNTP client connection
+static struct mg_connection *s_sntp_conn = NULL;
+
+/***
+ * Setup credentials for WiFi
+ * @param data
+ */
+void main_setconfig(void *data) {
+	struct mg_tcpip_driver_pico_w_data *d = (struct mg_tcpip_driver_pico_w_data *)data;
+	d->ssid = (char *)WIFI_SSID;
+	d->pass = (char *)WIFI_PASS;
+}
+
+/***
+ * Blink Status LED as call back from Mongoose
+ * @param arg
+ */
+static void blink_timer(void *arg) {
+  	(void) arg;
+  	cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, !cyw43_arch_gpio_get(CYW43_WL_GPIO_LED_PIN));
+
+	// Get the RTC date and time
+	datetime_t dt;
+	rtc_get_datetime(&dt);
+	/*MG_INFO(("RTC: %d-%d-%d %d:%d:%d\n",
+			dt.year,
+			dt.month,
+			dt.day,
+			dt.hour,
+			dt.min,
+			dt.sec));*/
+}
+
+// SNTP client callback
+static void sfn(struct mg_connection *c, int ev, void *ev_data) {
+	if (ev == MG_EV_SNTP_TIME) {
+		// Time received, the internal protocol handler updates what mg_now() returns
+		uint64_t curtime = mg_now();
+		MG_INFO(("SNTP-updated current time is: %llu ms from epoch", curtime));
+		// otherwise, you can process the server returned data yourself
+		{
+			uint64_t t = *(uint64_t *) ev_data;
+			datetime_t dt;
+			time_to_datetime(t / 1000, &dt);
+			MG_INFO(("Setting RTC to: %d-%d-%d %d:%d:%d\n",
+				dt.year,
+				dt.month,
+				dt.day,
+				dt.hour,
+				dt.min,
+				dt.sec));
+			rtc_set_datetime(&dt);
+			// Reset counter and refresh required flag
+			sntp_refresh_counter = 0;
+			sntp_refresh_required = false;
+		}
+	} else if (ev == MG_EV_CLOSE) {
+		s_sntp_conn = NULL;
+	}
+	(void) c;
+}
+
+// Called every 60 seconds - resets network state if stuck in DHCP REQUESTING or DOWN
+static void net_check_timer(void *arg) {
+	/* check state */
+	//MG_INFO(("State: %d", g_mgr.ifp->state));
+	if (g_mgr.ifp->state == MG_TCPIP_STATE_DOWN) {
+		// If interface is down, change it to MG_TCPIP_STATE_REQ, next run of this timer will bring it down and request connection again
+		g_mgr.ifp->state = MG_TCPIP_STATE_REQ;
+		MG_INFO(("State was MG_TCPIP_STATE_DOWN, reset state to MG_TCPIP_STATE_REQ"));
+	} else if (g_mgr.ifp->state == MG_TCPIP_STATE_REQ) {
+		// Reset interface status to down, mg_tcpip_poll will cause a reconnect
+		g_mgr.ifp->state = MG_TCPIP_STATE_DOWN;
+		MG_INFO(("State was MG_TCPIP_STATE_REQ, reset state to MG_TCPIP_STATE_DOWN"));
+	}
+}
+
+// Called every 5 seconds. Increase that for production case.
+static void sntp_timer(void *arg) {
+	// Check if refresh was at least 24h ago
+	if (sntp_refresh_counter > 8640)
+		sntp_refresh_required = true;
+	if (sntp_refresh_required) {
+		struct mg_mgr *mgr = (struct mg_mgr *) arg;
+		if (s_sntp_conn == NULL) { // connection issues a request
+			s_sntp_conn = mg_sntp_connect(mgr, NULL, sfn, NULL);
+		} else {
+			mg_sntp_request(s_sntp_conn);
+		}
+	}
+	sntp_refresh_counter++;
+}
+
+// https://forums.raspberrypi.com/viewtopic.php?t=312419
+uint8_t sys_dayOfWeek(datetime_t *dt) {
+    uint8_t day = dt->day; 
+    uint8_t month = dt->month; 
+    uint16_t year = dt->year;
+
+    // adjust month year
+    if (month < 3) {
+        month += 12;
+        year -= 1;
+    }
+
+    // split year
+    uint32_t c = year / 100;
+    year = year % 100;
+
+    // Zeller's congruence
+    return (c / 4 - 2 * c + year + year / 4 + 13 * (month + 1) / 5 + day - 1) % 7;
+}
+
+/***
+ * Main event callback handler for Mongoose
+ * @param c
+ * @param ev
+ * @param ev_data
+ */
+static void http_ev_handler(struct mg_connection *c, int ev, void *ev_data) {
+	if (ev == MG_EV_HTTP_MSG){
+		struct mg_http_message *hm = (struct mg_http_message *) ev_data;  // Parsed HTTP request
+		if (mg_match(hm->uri, mg_str("/websocket"), NULL)) {
+			// Upgrade to websocket. From now on, a connection is a full-duplex
+			// Websocket connection, which will receive MG_EV_WS_MSG events.
+			mg_ws_upgrade(c, hm, NULL);
+		} else if (mg_match(hm->uri, mg_str("/api"), NULL)) {
+			char *str_action = mg_json_get_str(hm->body, "$.action");
+			
+			if (strcmp(str_action, "get_status") == 0) {
+				MG_INFO(("Getting status"));
+				datetime_t dt;
+				rtc_get_datetime(&dt);
+				mg_http_reply(c, 200, "Content-Type: application/json\r\n", 
+					"\"status\": \"OK\","
+            		"\"current_day\": %d,"
+            		"\"current_time\": %d,"
+            		"\"heating_state\": 1,"
+            		"\"is_heating\": 1,"
+            		"\"boost_timer_countdown\": 0,"
+            		"\"timers\": [[127, 450, 390],[0, 420, 420],[0, 420, 420],[0, 420, 420],[0, 420, 420],[0, 420, 420]]"
+					"\n", 
+					sys_dayOfWeek(&dt), dt.hour * 60 + dt.min);
+			} else {
+				mg_http_reply(c, 400, "", "{\"status\": \"ERROR\", \"message\": \"Unkown action\"}\n", 123);
+				MG_INFO(("Unknown action"));
+			}
+			
+			mg_free(str_action);
+		} else {
+			struct mg_http_serve_opts opts = {
+				.root_dir = "/web",
+				.fs = &mg_fs_packed
+			 };
+			mg_http_serve_dir(c, hm, &opts);
+		}
+	} else if (ev == MG_EV_WS_MSG) {
+		// Got websocket frame. Received data is wm->data. Echo it back!
+		struct mg_ws_message *wm = (struct mg_ws_message *) ev_data;
+		mg_ws_send(c, wm->data.buf, wm->data.len, WEBSOCKET_OP_TEXT);
+	}
+}
+
+/***
+ * Initialise Mongoose and network
+ */
+void mongoose_init(void) {
+	mg_mgr_init(&g_mgr);      // Initialise event manager
+	mg_log_set(MG_LL_DEBUG);  // Set log level to debug
+
+	MG_INFO(("Starting HTTP listener"));
+	mg_http_listen(&g_mgr, HTTP_URL, http_ev_handler, NULL);
+}
+
+/***
+ * Poll Mongoose
+ */
+void mongoose_poll(void) {
+  	mg_mgr_poll(&g_mgr, 10);
+}
+
+/***
+ * Main
+ * @return
+ */
+int main(){
+	stdio_init_all();
+	sleep_ms(3000);
+
+	printf("Go\n");
+
+	// RTC init with default date and time
+	datetime_t dt;
+    dt.year = 2000;
+    dt.month = 1;
+    dt.day = 1;
+    dt.hour = 0;
+    dt.min = 0;
+    dt.sec = 0;
+	rtc_init();
+	rtc_set_datetime(&dt);
+
+	// do not access the CYW43 LED before Mongoose initializes !
+	MG_INFO(("Hardware initialised, starting firmware..."));
+	// This blocks forever. Call it at the end of main()
+	mongoose_init();
+	// This timer checks the time every 1s and enables/disables the relay
+	mg_timer_add(&g_mgr, 1000, MG_TIMER_REPEAT, blink_timer, NULL);
+	// This timer does an SNTP refresh. Refresh happens once a day, but timer checks if the time needs setting every 10s
+	mg_timer_add(&g_mgr, 10000, MG_TIMER_REPEAT | MG_TIMER_RUN_NOW, sntp_timer, &g_mgr);
+	// This timer is a network reset check
+	mg_timer_add(&g_mgr, 60000, MG_TIMER_REPEAT | MG_TIMER_RUN_NOW, net_check_timer, &g_mgr);
+	for (;;) {
+		mongoose_poll();
+	}
+
+	return 0;
+}
---
+++ web/heating.js
@@ -0,0 +1,264 @@
+var isChanging = false; // Used to prevent SSE updating controls when being edited
+var ws; // WebSocket
+
+// https://www.slingacademy.com/article/javascript-checking-if-a-tab-is-currently-focused-active/
+var hidden, visibilityChange;
+if (typeof document.hidden !== "undefined") {
+    hidden = "hidden";
+    visibilityChange = "visibilitychange";
+} else if (typeof document.msHidden !== "undefined") {
+    hidden = 'msHidden';
+    visibilityChange = 'msvisibilitychange';
+} else if (typeof document.webkitHidden !== "undefined") {
+    hidden = 'webkitHidden';
+    visibilityChange = 'webkitvisibilitychange';
+}
+
+// WebSocket called on load, streams from pico to browser
+function streamStatus() {
+    if (!document[hidden]) {
+        if (ws) { 
+            ws.close(); 
+            return; 
+        }
+        ws = new WebSocket(url.value);
+        if (!ws) return;
+
+        //ws.onopen = function() { log.innerHTML += 'CONNECTION OPENED<br/>'; }
+        ws.onmessage = function(ev) { 
+            updateStatus(ev.data);
+        }
+        ws.onerror = function(ev) { 
+            console.log(error);
+        }
+        ws.onclose = function() { 
+            console.log(error);
+            ws = null; 
+        }
+    }
+}
+
+// Legacy polling method (not used)
+function getStatus() {
+    const jsonData = {
+        "action": "get_status"
+    };
+    // Post back to the python service
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        updateStatus(this.responseText);
+    }
+    xhttp.open("POST", "/api", true);
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(JSON.stringify(jsonData));
+}
+
+// Populate the fields and controls with the current status from the Pico's JSON response
+function updateStatus(strRequest) {
+    var json_response = JSON.parse(strRequest);
+    console.log(json_response);
+
+    if (json_response.status == "OK") {
+        var dayOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+        document.getElementById("localTime").innerHTML = dayOfWeek[json_response.current_day - 1] + " " + formatTime(json_response.current_time) + " UTC";
+        document.getElementById("boostTimer").innerHTML = formatCountdown(json_response.boost_timer_countdown);
+        document.getElementById("heatingState").innerHTML = (json_response.heating_state ? "ENABLED" : "DISABLED");
+        document.getElementById("isHeating").innerHTML = (json_response.is_heating ? "ON" : "OFF");
+        if (!isChanging) {
+            const timerArr = json_response.timers;
+            var timer = 1;
+            for (var i = 0; i < timerArr.length; i++) {
+                timer = i + 1;
+                // Only set control if it is disabled (not editing)
+                if (document.getElementById("t" + timer + "Day1").disabled) {
+                    // Days
+                    checkTimerDayBoxes(timer, timerArr[i][0]);
+                    // On time
+                    document.getElementById("t" + timer + "On").innerHTML = formatTime(timerArr[i][1]);
+                    document.getElementById("t" + timer + "OnInput").value = timerArr[i][1];
+                    // Off time
+                    document.getElementById("t" + timer + "Off").innerHTML = formatTime(timerArr[i][2]);
+                    document.getElementById("t" + timer + "OffInput").value = timerArr[i][2];
+                }
+            }
+        }
+    }
+}
+
+// Functions to prevent the interval resetting displayed values when changing a control
+function startChange() {
+    isChanging = true;
+}
+
+function endChange() {
+    isChanging = false;
+}
+
+// Global heating enable/disable
+function triggerHeating() {
+    const jsonData = {
+        "action": "trigger_heating"
+    };
+    // Post back to the python service
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        var json_response = JSON.parse(this.responseText);
+        console.log(json_response);
+
+        if (json_response.status == "OK") {
+            // reset led indicator to none
+            document.getElementById("heatingState").innerHTML = (json_response.heating_state ? "ENABLED" : "DISABLED");
+        } else {
+            alert("Error setting heating state");
+        }
+    }
+    xhttp.open("POST", "/api", true);
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(JSON.stringify(jsonData));
+}
+
+// Set the target temperature
+function triggerBoost() {
+    const jsonData = {
+        "action": "boost"
+    };
+    // Post back to the python service
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        var json_response = JSON.parse(this.responseText);
+        console.log(json_response);
+
+        if (json_response.status == "OK") {
+            // reset led indicator to none
+            document.getElementById("boostTimer").innerHTML = formatCountdown(json_response.boost_timer_countdown);
+        } else {
+            alert("Error setting target temperature");
+        }
+    }
+    xhttp.open("POST", "/api", true);
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(JSON.stringify(jsonData));
+}
+
+function checkTimerDayBoxes(timer, newTimerDays) {
+    // Based on the binary days setting, check or uncheck each day checkbox
+    bMask = 1; // Mask starts at 1, and is then left shifted in the loop
+    // Loop from 1 to 7 - 1 = Monday
+    for (var i = 1; i < 8; i++) {
+        // If the bit in newTimerDays is the same bit set in bMask, then check the box
+        document.getElementById("t" + timer + "Day" + i).checked = newTimerDays & bMask;
+        // Shift the mask bit left each time (zero filled from the right)
+        bMask = bMask << 1;
+    }
+}
+
+
+// This function is used when the control slider is dragged
+function moveTime(timer, onOrOff) {
+    document.getElementById("t" + timer + onOrOff).innerHTML = formatTime(document.getElementById("t" + timer + onOrOff + "Input").value);
+}
+
+// Used by above functions to format the set time into 12h format hh:mm
+function formatTime(timeIn) {
+    var hour = Math.floor(timeIn / 60)
+    var ampm = " AM"
+    if (hour > 11)
+        ampm = " PM"
+    if (hour > 12)
+        hour -= 12
+    return String(hour) + ":" + String(timeIn % 60).padStart(2, "0") + ampm;
+}
+
+// Used by above functions to format the boost countdown into mm:ss format
+function formatCountdown(countdownIn) {
+    return String(Math.floor(countdownIn / 60)).padStart(2, "0") + ":" + String(countdownIn % 60).padStart(2, "0");
+}
+
+function editTimer(timer) {
+    // Check state of a control
+    if (document.getElementById("t" + timer + "Day1").disabled) {
+        startChange();
+        // Enable controls
+        toggleControlsDisabled(timer, false);
+        // Show cancel button
+        document.getElementById("btnC" + timer).style.display="block";
+        // Change to save icon
+        document.getElementById("btnT" + timer).innerHTML = "&#x1F4BE;";
+    } else {
+        var newDays = 0;
+        var daysTest = 1;
+        // Loop from 1 to 7 - 1 = Monday
+        for (var i = 1; i < 8; i++) {
+            // If the day is checked, add on the test byte
+            if (document.getElementById("t" + timer + "Day" + i).checked)
+                newDays += daysTest;
+            daysTest <<= 1; // Shift bit left in the test byte
+        }
+
+        // Apply the changes
+        const jsonData = {
+            "action": "set_timer",
+            "timer_number": timer,
+            "new_days": newDays,
+            "new_on_time": document.getElementById("t" + timer + "OnInput").value,
+            "new_off_time": document.getElementById("t" + timer + "OffInput").value
+        };
+        // Post back to the python service
+        const xhttp = new XMLHttpRequest();
+        xhttp.onload = function() {
+            var json_response = JSON.parse(this.responseText);
+            console.log(json_response);
+
+            if (json_response.status != "OK") {
+                alert("Error setting timer: " + json_response.message);
+            }
+        }
+        xhttp.open("POST", "/api", true);
+        xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhttp.send(JSON.stringify(jsonData));
+
+        // Disable controls
+        toggleControlsDisabled(timer, true);
+        // Change to edit icon
+        document.getElementById("btnT" + timer).innerHTML = "&#x1F4DD;";
+        // Hide cancel button
+        document.getElementById("btnC" + timer).style.display="none";
+        // Delay resuming the SSE by over a second, allowing time for the Pico to receive and response with the new state
+        setTimeout(endChange(), 1200);
+    }
+}
+
+function cancelTimer(timer) {
+    // Disable controls
+    toggleControlsDisabled(timer, true);
+    // Change to edit icon
+    document.getElementById("btnT" + timer).innerHTML = "&#x1F4DD;";
+    // Hide cancel button
+    document.getElementById("btnC" + timer).style.display="none";
+    endChange();
+}
+
+function toggleControlsDisabled(timer, isDisabled) {
+    // Loop from 1 to 7 - 1 = Monday
+    for (var i = 1; i < 8; i++) {
+        // If the bit in newTimerDays is the same bit set in bMask, then check the box
+        document.getElementById("t" + timer + "Day" + i).disabled = isDisabled;
+    }
+    // On time
+    document.getElementById("t" + timer + "OffInput").disabled = isDisabled;
+    // On time
+    document.getElementById("t" + timer + "OnInput").disabled = isDisabled;
+}
+
+// These events will start the server side event source to stream status
+// This one is for mobiles when the browser/tab resumes
+document.addEventListener("visibilitychange", streamStatus, false);
+window.addEventListener('beforeunload', () => {
+	if (ws)
+        ws.close();
+});
+
+// For desktops when tab is focused
+//document.addEventListener("focus", streamStatus, false);
+// For initial window load
+window.addEventListener("load", streamStatus);
